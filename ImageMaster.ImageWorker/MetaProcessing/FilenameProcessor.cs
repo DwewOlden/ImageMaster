d1f25f6eacf9070c96d7ad1e1b1c895b299466c8
Andrew Olden <Andrew.Olden@outlook.com>
2019-08-27 19:44:41 +0100

    more file processing work

---
+++ ImageMaster.ImageWorker/MetaProcessing/FilenameProcessor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageMaster.ImageWorker.MetaProcessing
+{
+    public class FilenameProcessor
+    {
+        public bool CreateDirectory(string path,bool includeRaw)
+        {
+            string directoryName = GetDirectoryNameString();
+            string completepath = System.IO.Path.Combine(path, directoryName);
+
+            return true;
+
+
+        }
+
+        private string GetDirectoryNameString()
+        {
+            DateTime dateTime = DateTime.Now;
+            string name = dateTime.ToString("dd MM yyyy");
+
+            return name;
+        }
+    }
+}
